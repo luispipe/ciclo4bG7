ccaabe1356fdc30efa2283c04a3f81aacdc7f563
luispipe <lflondonor@unal.edu.co>
2022-10-28 18:14:29 -0500

    Second commit

--- app/src/main/java/com/example/unleeg7/View/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/unleeg7/View/ui/activities/LoginActivity.kt
@@ -10,7 +10,7 @@ class LoginActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-       // binding= ActivityMainBinding.inflate(layoutInflater)
+        // binding= ActivityMainBinding.inflate(layoutInflater)
         //setContentView(binding.root)
     }
 }
