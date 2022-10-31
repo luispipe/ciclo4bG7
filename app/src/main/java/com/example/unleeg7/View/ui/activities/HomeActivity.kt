7afa83c12b93a6b9f7954f7483c8a0fb5f8b4b38
luispipe <lflondonor@unal.edu.co>
2022-10-31 18:56:02 -0500

    menus-part1

--- app/src/main/java/com/example/unleeg7/View/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/example/unleeg7/View/ui/activities/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.unleeg7.View.ui.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.example.unleeg7.R
 
@@ -8,5 +10,11 @@ class HomeActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.nav_book,menu)
+        return true
     }
 }
