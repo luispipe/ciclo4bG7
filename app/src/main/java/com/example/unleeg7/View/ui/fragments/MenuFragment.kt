b7b611d9c84771c61e517a5ede6d1fe44bc70495
luispipe <lflondonor@unal.edu.co>
2022-11-02 18:28:50 -0500

    navigation

--- app/src/main/java/com/example/unleeg7/View/ui/fragments/MenuFragment.kt
+++ app/src/main/java/com/example/unleeg7/View/ui/fragments/MenuFragment.kt
@@ -5,30 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import androidx.navigation.fragment.findNavController
 import com.example.unleeg7.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MenuFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MenuFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +20,38 @@ class MenuFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_menu, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val cardBook= view.findViewById<CardView>(R.id.fragBooks)
+        cardBook.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_booksFragment)
+        }
+
+        val cardShop= view.findViewById<CardView>(R.id.fragShop)
+        cardShop.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_shopFragment)
+        }
+        val cardFavorite= view.findViewById<CardView>(R.id.fragFavorite)
+        cardFavorite.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_favoritesFragment)
+        }
+
+        val cardProfile= view.findViewById<CardView>(R.id.fragProfile)
+        cardProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_profileFragment)
+        }
+
+        val cardShip= view.findViewById<CardView>(R.id.fragShipping)
+        cardShip.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_shippingFragment)
+        }
+
+        val cardHelp= view.findViewById<CardView>(R.id.fragHelp)
+        cardHelp.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_helpFragment)
+        }
+
     }
+
+
 }
